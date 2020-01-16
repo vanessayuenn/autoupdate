e2a00a0fa9986894ab96ea65fe796f4d7ef1dcd3
Sara T <sara.tan@lendingworks.co.uk>
2020-01-16 15:27:32 +0000

    Add debug lines

--- src/autoupdater.js
+++ src/autoupdater.js
@@ -160,6 +160,8 @@ class AutoUpdater {
       }
 
       for (const label of pull.labels) {
+        ghCore.debug(`Label on PR: ${label.name}`);
+        ghCore.debug(`Label(s) specified in config: ${labels}`);
         if (labels.includes(label.name)) {
           ghCore.info(
             `Pull request has label '${label.name}' and PR branch is behind base branch.`
